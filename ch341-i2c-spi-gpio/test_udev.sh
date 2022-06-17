664ffc5c78e2c3ce5ea65dc588830b532daf7317
Your Name <you@example.com>
2022-06-17 16:22:09 -0400

    2022_0617_1622pm_09

---
+++ ch341-i2c-spi-gpio/test_udev.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "$0 ==== start" >> /tmp/log.001.test.4.${USEC_INITIALIZED}.txt
+set                  >> /tmp/log.001.test.4.${USEC_INITIALIZED}.txt
+echo "$0 ==== end"   >> /tmp/log.001.test.4.${USEC_INITIALIZED}.txt
