980efd7acbf8c62d0bf78cb606192933b5266982
Your Name <you@example.com>
2022-06-24 14:12:47 -0400

    2022_0624_1412pm_47

--- linux_i2c01/main_linux_i2c01.c
+++ linux_i2c01/main_linux_i2c01.c
@@ -46,6 +46,20 @@ bool stusb4500_read_byte_test( STUSB4500_ST * __st45LP ){
     printf( " 81939918383 : get from Reg <0x%0x> : <%0x> \n" , __st45LP->wBuf[0] , __ch[0] ) ;
     gap01();
 
+    __ch = 
+        i2c_reg_read_bytes( __st45LP, 0x06, 4 ) ;
+    if ( NULL == __ch ) return false ;
+    printf( " 81939918385 : get from Reg <0x%0x> : <%0x> <%0x> <%0x> <%0x> \n" , __st45LP->wBuf[0] 
+            , __ch[0] , __ch[1] , __ch[2] , __ch[3] ) ;
+    gap01();
+
+    __ch = 
+        i2c_reg_read_bytes( __st45LP, 0x08, 4 ) ;
+    if ( NULL == __ch ) return false ;
+    printf( " 81939918385 : get from Reg <0x%0x> : <%0x> <%0x> <%0x> <%0x> \n" , __st45LP->wBuf[0] 
+            , __ch[0] , __ch[1] , __ch[2] , __ch[3] ) ;
+    gap01();
+
     return true ;
 } // stusb4500_read_byte_test
 
--- stusb_4500_in_c/i2c_reg_write.h
+++ stusb_4500_in_c/i2c_reg_write.h
@@ -8,6 +8,7 @@ bool _i2c_reg_write_bytes(STUSB4500_ST * __st45LP, char __i2cRegAddr, char * ___
 bool _i2c_reg_write_one_byte(STUSB4500_ST * __st45LP, char __i2cRegAddr, char d) ;
 
 char * i2c_reg_read_one_byte(STUSB4500_ST * __st45LP, char __i2cRegAddr) ;
+char * i2c_reg_read_bytes(STUSB4500_ST * ___st45LP, char ___i2cRegAddr, size_t ___len ) ;
 
 #endif
 
