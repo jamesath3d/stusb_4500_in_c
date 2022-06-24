b0c275c22cb6d3f774e0654a5f2b5711a5d6cbde
Your Name <you@example.com>
2022-06-24 13:22:58 -0400

    2022_0624_1322pm_57

--- stusb_4500_in_c/i2c_reg_write.c
+++ stusb_4500_in_c/i2c_reg_write.c
@@ -4,35 +4,50 @@
 
 
 // _i2c_reg_write_one_byte performs the following write I2C transaction:
-//   register address (__i2cRegAddr), register value (d)
+//   register address (___i2cRegAddr), register value (d)
 // This is a common way of writing into I2C device registers.
 // Returns true if successful, false otherwise.
-bool _i2c_reg_write_bytes(STUSB4500_ST * __st45LP, char __i2cRegAddr, char * ___buf, size_t ___len) {
+bool _i2c_reg_write_bytes(STUSB4500_ST * ___st45LP, char ___i2cRegAddr, char * ___buf, size_t ___len) {
 
     if ( ___len > 0x80 ) return false ;
 
-    size_t __len = ___len ;
+    //uint8_t __buf[2] = {___i2cRegAddr, d}; // put the regAddr and dataByte into the array.
+    ___st45LP -> wBuf[0] = ___i2cRegAddr ;
+    memcpy ( ___st45LP -> wBuf + 1 , ___buf , ___len ) ;
+    ___st45LP -> wLEN = ___len + 1 ;
+    ___st45LP -> rLEN = 0 ;
 
-  //uint8_t __buf[2] = {__i2cRegAddr, d}; // put the regAddr and dataByte into the array.
-  __st45LP -> wBuf[0] = __i2cRegAddr ;
-  memcpy ( __st45LP -> wBuf + 1 , ___buf , __len ) ;
-  __st45LP -> wLEN = ___len + 1 ;
-  __st45LP -> rLEN = 0 ;
-
-  return 
-      _i2c_tx1( __st45LP );
+    return 
+        _i2c_tx1( ___st45LP );
 
 } // _i2c_reg_write_bytes
 
-bool _i2c_reg_write_one_byte(STUSB4500_ST * __st45LP, char __i2cRegAddr, char d) {
+bool _i2c_reg_write_one_byte(STUSB4500_ST * ___st45LP, char ___i2cRegAddr, char d) {
 
-  char __buf[1] = {d} ;
-  return 
-      _i2c_reg_write_bytes( __st45LP, __i2cRegAddr, __buf , 1 );
+    char __buf[1] = {d} ;
+    return 
+        _i2c_reg_write_bytes( ___st45LP, ___i2cRegAddr, __buf , 1 );
 
 } // _i2c_reg_write_one_byte
 
-char * i2c_reg_read_one_byte(STUSB4500_ST * __st45LP, char __i2cRegAddr) {
-    return __st45LP -> rBuf ; 
+
+
+
+
+char * i2c_reg_read_bytes(STUSB4500_ST * ___st45LP, char ___i2cRegAddr, size_t ___len ) {
+
+    if ( ___len > 0x80 ) return false ;
+
+    ___st45LP -> wBuf[0] = ___i2cRegAddr ;
+    //memcpy ( ___st45LP -> wBuf + 1 , ___buf , ___len ) ;
+    ___st45LP -> wLEN = 1 ;
+    ___st45LP -> rLEN = ___len ;
+
+    return ___st45LP -> rBuf ; 
     //return NULL ;
+} // i2c_reg_read_bytes
+
+char * i2c_reg_read_one_byte(STUSB4500_ST * ___st45LP, char ___i2cRegAddr) {
+    return 
+        i2c_reg_read_bytes( ___st45LP, ___i2cRegAddr, 1 ) ;
 } // i2c_reg_read_one_byte
