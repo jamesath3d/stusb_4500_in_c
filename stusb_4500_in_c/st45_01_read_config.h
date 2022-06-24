0a37164d813820a089845b9c276035ad91c7eb13
Your Name <you@example.com>
2022-06-24 16:34:35 -0400

    2022_0624_1634pm_34

--- stusb_4500_in_c/st45_01_read_config.h
+++ stusb_4500_in_c/st45_01_read_config.h
@@ -4,6 +4,14 @@
 
 #include "st45_00_all.h"
 
+typedef struct {
+  uint16_t v1; // Voltage for first preference - mapping in read_config()
+  uint8_t i1; // Current for first preference - mapping in current lookup table of STUSB4500 datasheet
+  uint16_t v2; // Voltage for second preference - mapping in read_config()
+  uint8_t i2; // Current for second preference - mapping in current lookup table of STUSB4500 datasheet
+  bool req_pd; // If true, requires source to be PD capable before enabling output
+} ST45_config;
+
 
 #endif
 
