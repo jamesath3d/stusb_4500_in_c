a6ed482649962b7845ae6bcf0b8a31fb86264c60
Your Name <you@example.com>
2022-06-27 14:46:08 -0400

    2022_0627_1446pm_08

--- stusb_4500_in_c/st45_32_nvr_enter_read.c
+++ stusb_4500_in_c/st45_32_nvr_enter_read.c
@@ -6,18 +6,18 @@
 int _st45_enter_nvr_read( STUSB4500_ST * __st45LP ) {
     unsigned char Buffer[2];
     
-    Buffer[0]=FTP_CUST_PASSWORD;  /* Set Password*/
-    if ( I2C_Write_USB_PD(Port,FTP_CUST_PASSWORD_REG,Buffer,1)  != HAL_OK )return -1;
+    //Buffer[0]=FTP_CUST_PASSWORD;  /* Set Password*/
+    if ( false == _i2c_reg_write_one_byte( __st45LP , FTP_CUST_PASSWORD_REG, FTP_CUST_PASSWORD)  )return -1;
     
     {  
         //NVM Power-up Sequence
         //After STUSB start-up sequence, the NVM is powered off.
         
-        Buffer[0]= 0;  /* NVM internal controller reset */
-        if ( I2C_Write_USB_PD(Port,FTP_CTRL_0,Buffer,1)  != HAL_OK ) return -1;
+        //Buffer[0]= 0;  /* NVM internal controller reset */
+        if ( false == _i2c_reg_write_one_byte( __st45LP , FTP_CTRL_0, 0x0 )  ) return -1;
         
-        Buffer[0]= FTP_CUST_PWR | FTP_CUST_RST_N; /* Set PWR and RST_N bits */
-        if ( I2C_Write_USB_PD(Port,FTP_CTRL_0,Buffer,1) != HAL_OK ) return -1;
+        //Buffer[0]= FTP_CUST_PWR | FTP_CUST_RST_N; /* Set PWR and RST_N bits */
+        if ( false == _i2c_reg_write_one_byte( __st45LP , FTP_CTRL_0, (FTP_CUST_PWR | FTP_CUST_RST_N) ) ) return -1;
     }
     
     return 0 ;
