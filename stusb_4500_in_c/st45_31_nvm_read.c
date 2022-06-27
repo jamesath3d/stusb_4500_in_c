accd0ea330f8e9f9070b57c24aaef79d925c7eaa
Your Name <you@example.com>
2022-06-27 14:10:41 -0400

    2022_0627_1410pm_41

--- stusb_4500_in_c/st45_31_nvm_read.c
+++ stusb_4500_in_c/st45_31_nvm_read.c
@@ -6,11 +6,31 @@ char _st45_rBuf[5][8] = {};
 char* _st45_read_top(void) {
     //int __rt = nvm_read( &(_st45_rBuf[0][0]), sizeof(_st45_rBuf) );
     int __rt = -1 ;
-    if(__rt != 0) //Error
+
+    if ( 1 ) {
+        for ( uint8_t __ii = 0 ; __ii < 5 ; __ii ++ ) {
+            for ( uint8_t __jj = 0 ; __jj < 8 ; __jj ++ ) {
+                if ( 0 == __jj ) {
+                    printf ( "%02x"  , _st45_rBuf[__ii][__jj] );
+                } else {
+                    printf ( " %02x" , _st45_rBuf[__ii][__jj] );
+                }
+            }
+            if ( 2 == __ii ) {
+                printf( "\n\n" ) ;
+            } else {
+                printf( "\n" ) ;
+            }
+        }
+
+    }
+
+    if(__rt < 0) //Error
     {
         printf("819399111 : Error NVM read\r\n");
         return NULL ;
     }
+
     return &(_st45_rBuf[0][0]) ;
 } // _st45_read_top(void) {
 
