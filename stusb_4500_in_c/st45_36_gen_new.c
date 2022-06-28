c33b09a40abd3518410e720df899689fe7e19668
Your Name <you@example.com>
2022-06-28 15:59:33 -0400

    2022_0628_1559pm_33

--- stusb_4500_in_c/st45_36_gen_new.c
+++ stusb_4500_in_c/st45_36_gen_new.c
@@ -7,11 +7,10 @@ char* _st45_gen_new_config( uint16_t __Iconfig1,
         uint16_t __Iconfig3, uint16_t __Vconfig3) {
 
     static char __st45new[ST45_NVM_SIZE] ;
-
     char* __rt1 = __st45new;
 
+    //memset( __st45new, 0, ST45_NVM_SIZE );
     memcpy( __st45new, _st45Default, ST45_NVM_SIZE );
-    memset( __st45new, 0, ST45_NVM_SIZE );
 
 
     return __rt1 ;
