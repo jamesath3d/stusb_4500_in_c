4758e6ac31d2384144d7d4ca1b4ec4611a13448f
Your Name <you@example.com>
2022-06-28 12:02:42 -0400

    2022_0628_1202pm_42

--- stusb_4500_in_c/st45_33_nvm_cmp.c
+++ stusb_4500_in_c/st45_33_nvm_cmp.c
@@ -5,13 +5,15 @@ const char _st45Default[] = ST45default ;
 bool _st45_comp_buf_with_defult( char* ___cmpBuf ) {
     bool __rt1 ;
     bool __rt2 = true ;
+    uint8_t __ii ;
+    uint8_t __jj ;
     uint8_t __kk ;
     printf("834881811 :                         ========== NVM cmp : begin \n");
     if ( 1 ) {
-        for ( uint8_t __ii = 0 ; __ii < 5 ; __ii ++ ) {
+        for ( __ii = 0 ; __ii < 5 ; __ii ++ ) {
             __rt1 = true ;
             printf ( "cmp  : %01d : "  , __ii );
-            for ( uint8_t __jj = 0 ; __jj < 8 ; __jj ++ ) {
+            for ( __jj = 0 ; __jj < 8 ; __jj ++ ) {
                 __kk = __ii*8 + __jj ;
                 printf ( " %02hhX" , ___cmpBuf[__kk] );
                 if ( ___cmpBuf[__kk] != _st45Default[__kk] ) {
@@ -19,11 +21,19 @@ bool _st45_comp_buf_with_defult( char* ___cmpBuf ) {
                     printf ( "(%02hhX)" , _st45Default[__kk] );
                 }
             }
+            if ( ! __rt1 ) {
+            }
             if ( __rt1 ) {
                 printf( " -- ok \n" ) ;
             } else {
                 printf( " -- failed 01 \n" ) ;
                 __rt2 = false ;
+                printf ( "orig : %01d : "  , __ii );
+                for ( __jj = 0 ; __jj < 8 ; __jj ++ ) {
+                    __kk = __ii*8 + __jj ;
+                    printf ( " %02hhX" , _st45Default[__kk] ) ;
+                }
+                printf( " -- default \n" ) ;
             }
         }
 
