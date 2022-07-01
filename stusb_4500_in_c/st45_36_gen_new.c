3f538a7e569fc46e5610271b271d076990088035
Your Name <you@example.com>
2022-07-01 09:43:17 -0400

    2022_0701_0943am_17

--- stusb_4500_in_c/st45_36_gen_new.c
+++ stusb_4500_in_c/st45_36_gen_new.c
@@ -1,9 +1,9 @@
 #include "st45_00_all.h"
 
 
-extern const uint8_t _st45Default[] ;
+//extern const uint8_t _st45Default[] ;
 //static uint8_t _st45new[5][8] ;
-static ST45config _st45newConfig ;
+//static ST45config _st45newConfig ;
 
 ST45config* _st45_gen_new_config( ST45config* ___st45config,
         uint8_t ___lowVp1, uint8_t ___upVp1, float __Iconfig1, 
@@ -11,11 +11,11 @@ ST45config* _st45_gen_new_config( ST45config* ___st45config,
         uint8_t ___lowVp3, uint8_t ___upVp3, float __Iconfig3, float __Vconfig3) {
     if ( NULL == ___st45config ) return NULL ;
 
-    _st45_check_and_fill_config( &(_st45newConfig . pdo[0]) , 5,          __Iconfig1 , ___lowVp1, ___upVp1 ) ;
-    _st45_check_and_fill_config( &(_st45newConfig . pdo[1]) , __Vconfig2, __Iconfig2 , ___lowVp2, ___upVp2 ) ;
-    _st45_check_and_fill_config( &(_st45newConfig . pdo[2]) , __Vconfig3, __Iconfig3 , ___lowVp3, ___upVp3 ) ;
+    _st45_check_and_fill_config( &(___st45config -> pdo[0]) , 5,          __Iconfig1 , ___lowVp1, ___upVp1 ) ;
+    _st45_check_and_fill_config( &(___st45config -> pdo[1]) , __Vconfig2, __Iconfig2 , ___lowVp2, ___upVp2 ) ;
+    _st45_check_and_fill_config( &(___st45config -> pdo[2]) , __Vconfig3, __Iconfig3 , ___lowVp3, ___upVp3 ) ;
 
-    _st45newConfig . pdoAmount = 3 ;
+    ___st45config -> pdoAmount = 3 ;
 
     return ___st45config ;
 } // _st45_gen_new_config
