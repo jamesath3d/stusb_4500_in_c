b8af3aa7f2eb7187950b6886abee735d41cd3ed6
Your Name <you@example.com>
2022-07-01 12:01:21 -0400

    2022_0701_1201pm_21

--- stusb_4500_in_c/st45_3b_convert_config_to_nvm_buf.c
+++ stusb_4500_in_c/st45_3b_convert_config_to_nvm_buf.c
@@ -3,7 +3,7 @@
 
 ST45config* _st45_convert_config_to_nvm_buf(ST45config* ___st45config) {
     uint16_t __u01 ;
-    //uint16_t __u02 ;
+    uint16_t __u02 ;
     if ( NULL == ___st45config ) return NULL ;
 
     ___st45config -> buf[3][2] &= (~0b110) ;
@@ -14,6 +14,22 @@ ST45config* _st45_convert_config_to_nvm_buf(ST45config* ___st45config) {
     //PDO2v : Load voltage (10-bit)
     // -bit 9:2 - sector 4, byte 1, bits 0:7
     // -bit 0:1 - sector 4, byte 0, bits 6:7
+    __u01 = ___st45config -> pdo[1] . Vu ;
+    __u02 = __u01 & 0b11 ;
+    __u02 <<= 6 ;
+    ___st45config -> buf[4][0] &= (~(0b11 <<6)) ;
+    ___st45config -> buf[4][0] |= __u02 ;
+    __u02 = __u01 >> 2 ;
+    ___st45config -> buf[4][1] = __u02 ;
+    //PDO3v : Load voltage (10-bit)
+    // -bit 8:9 - sector 4, byte 3, bits 0:1
+    // -bit 0:7 - sector 4, byte 2, bits 0:7
+    __u01 = ___st45config -> pdo[2] . Vu ;
+    __u02 = __u01 >> 8;
+    ___st45config -> buf[4][3] &= (~0b11) ;
+    ___st45config -> buf[4][3] |= (0b11 & __u02) ;
+    ___st45config -> buf[4][2] = 0xFFUL & __u02 ;
+
 
 
     return ___st45config ;
