ba52db366b9ad958e2d88a6b11f57953037f1f75
Your Name <you@example.com>
2022-07-02 15:17:41 -0400

    2022_0702_1517pm_41

--- stusb_4500_in_c/st45_3c_exit_test_mode.c
+++ stusb_4500_in_c/st45_3c_exit_test_mode.c
@@ -8,12 +8,14 @@ bool _st45_exit_test_mode( ST45i2cST * ___st45I2C) {
     __buf[0]= FTP_CUST_RST_N;
     __buf[1]= 0x00;  
     if ( false == 
-            _i2c_reg_write_bytes( ___st45I2C, FTP_CTRL_0, __buf, 2) ) 
+            _i2c_reg_write_bytes( ___st45I2C, 
+                FTP_CTRL_0, __buf, 2) ) 
         return false ;
 
     // Clear Password
     if ( false == 
-            _i2c_reg_write_one_byte( ___st45I2C, FTP_CUST_PASSWORD_REG, 0x00 ) )
+            _i2c_reg_write_one_byte( ___st45I2C, 
+                FTP_CUST_PASSWORD_REG, 0x00 ) )
         return false ;  
 
     return true ;
