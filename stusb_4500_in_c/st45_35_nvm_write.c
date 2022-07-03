5aba7c3266feff6377a5884854584b78e276fd54
Your Name <you@example.com>
2022-07-02 22:11:27 -0400

    2022_0702_2211pm_27

--- stusb_4500_in_c/st45_35_nvm_write.c
+++ stusb_4500_in_c/st45_35_nvm_write.c
@@ -3,6 +3,8 @@
 
 int _st45_nvm_write( ST45i2cST * ___st45I2C, uint8_t * ___wBufTo ) {
     static char __wBuf[5][8]; // must read all 40 bytes.
+    uint8_t __u01 ;
+    uint8_t __u02 ;
 
     if ( NULL == ___st45I2C || NULL == ___wBufTo ) {
         FP1( "818328191 : parameter NULL \n" );
@@ -10,10 +12,11 @@ int _st45_nvm_write( ST45i2cST * ___st45I2C, uint8_t * ___wBufTo ) {
 
     memcpy(__wBuf,___wBufTo,sizeof(__wBuf));
 
-    uint8_t __u01 = SECTOR_0 | SECTOR_1  | SECTOR_2 | SECTOR_3  | SECTOR_4 ;
-    if ( 3 != _st45_enter_nvm_write( ___st45I2C , __u01 ) ) EXi( -1 ) ;
+    __u01 = SECTOR_0 | SECTOR_1  | SECTOR_2 | SECTOR_3  | SECTOR_4 ;
+    __u02 = _st45_enter_nvm_write( ___st45I2C , __u01 ) ;
+    if ( 10 != __u02 ) EXi( __u02 ) ;
 
-    if ( 8 != _st45_nvm_write_8_bytes(___st45I2C, 0, &(__wBuf[0][0])) ) EXi( -1 ) ;
+    if ( 8 != _st45_nvm_write_8_bytes(___st45I2C, 0, &(__wBuf[0][0])) ) EXi( -8 ) ;
 
     /*
     if ( 3 != _st45_enter_nvm_read( ___st45I2C ) ) return -1;
--- stusb_4500_in_c/st45_3d_nvm_enter_write.c
+++ stusb_4500_in_c/st45_3d_nvm_enter_write.c
@@ -10,33 +10,34 @@ int _st45_enter_nvm_write( ST45i2cST * ___st45LP , unsigned char ___erasedSector
 
     __cnt = 0 ;
 
-    __u01 = FTP_CUST_PASSWORD; // FTP_CUST_PASSWORD;  Set Password : 0x95 : 0x47
+    __u01 = FTP_CUST_PASSWORD; // FTP_CUST_PASSWORD;  Set Password : 0x95 w: 0x47
     if ( false == _i2c_reg_write_one_byte( ___st45LP , FTP_CUST_PASSWORD_REG,
                 __u01 )  ) EXi( -201 ) ;
-    __cnt ++ ;
+    __cnt ++ ; // 1
 
 
     
-    __u01 = 0x00 ; // this register must be NULL for Partial Erase feature 
+    // this register must be NULL for Partial Erase feature : 0x53 w: 0x00
+    __u01 = 0x00 ; 
     if ( false == _i2c_reg_write_one_byte( ___st45LP , RW_BUFFER,
                 __u01)  ) EXi( -211 ) ;
-    __cnt ++ ;
+    __cnt ++ ; // 2
 
     
     {  
         //NVM Power-up Sequence
         //After STUSB start-up sequence, the NVM is powered off.
         
-        __u01 = 0x00 ; // NVM internal controller reset //
+        __u01 = 0x00 ; // NVM internal controller reset // 0x96 w: 0x00
         if ( false == _i2c_reg_write_one_byte( ___st45LP , FTP_CTRL_0,
                     __u01)  ) EXi( -213 ) ;
-        __cnt ++ ;
+        __cnt ++ ; // 3
 
 
         __u01 = FTP_CUST_PWR | FTP_CUST_RST_N; // Set PWR and RST_N bits 
         if ( false == _i2c_reg_write_one_byte( ___st45LP , FTP_CTRL_0,
                     __u01) ) EXi( -214 ) ;
-        __cnt ++ ;
+        __cnt ++ ; // 4 // 0x96 w: 0xC0
     }
 
 
@@ -44,60 +45,61 @@ int _st45_enter_nvm_write( ST45i2cST * ___st45LP , unsigned char ___erasedSector
     // Load 0xF1 to erase all sectors of FTP and Write SER Opcode 
     if ( false == _i2c_reg_write_one_byte( ___st45LP , FTP_CTRL_1,
                 __u01) ) EXi( -215 ) ; // Set Write SER Opcode 
-    __cnt ++ ;
+    __cnt ++ ; // 5 // 0x97 w:0xFA
 
     __u01 =FTP_CUST_PWR | FTP_CUST_RST_N | FTP_CUST_REQ ; 
     if ( false == _i2c_reg_write_one_byte( ___st45LP , FTP_CTRL_0,
                 __u01) ) EXi( -217 ) ; // Load Write SER Opcode 
-    __cnt ++ ;
+    __cnt ++ ; // 6 // 0x96 w: 0xD0
 
     do 
     {
-        // Wait for execution 1
+        // Wait for execution 1 // read from 0x96
         if ( NULL == _i2c_reg_read_one_byte( ___st45LP , FTP_CTRL_0 ) ) EXi( -218 );
     }
-    while(___st45LP->rBuf[0]  & FTP_CUST_REQ); 
+    while(___st45LP->rBuf[0]  & FTP_CUST_REQ);  // 1st read got : 0xC0, mask 0x10, exit loop
 
     __u01 =  SOFT_PROG_SECTOR & FTP_CUST_OPCODE_MASK ;  
     if ( false == _i2c_reg_write_one_byte( ___st45LP , FTP_CTRL_1,
-                __u01) ) EXi( -221 ) ; 
-    __cnt ++ ; // Set Soft Prog Opcode 
+                __u01) ) EXi( -221 ) ; // Set Soft Prog Opcode 
+    __cnt ++ ; // 7 // 0x97 w: 0x07
 
     __u01 =FTP_CUST_PWR | FTP_CUST_RST_N | FTP_CUST_REQ ; 
     // Load Soft Prog Opcode 
     if ( false == _i2c_reg_write_one_byte( ___st45LP , FTP_CTRL_0,
                 __u01) ) EXi( -221 ) ; 
-    __cnt ++ ; // Set Soft Prog Opcode 
+    __cnt ++ ; // Set Soft Prog Opcode // 0x96 w: 0xD0 // 8
 
 
     do 
     {
-        // Wait for execution 2
+        // Wait for execution 2 // read from 0x96
         if ( NULL == _i2c_reg_read_one_byte( ___st45LP , FTP_CTRL_0 ) ) EXi( -223 );
     }
-    while(___st45LP->rBuf[0]  & FTP_CUST_REQ);
+    while(___st45LP->rBuf[0]  & FTP_CUST_REQ); // read 0xD0 , failed ; read 0xC0, ok, exit loop
 
     __u01 = ERASE_SECTOR & FTP_CUST_OPCODE_MASK ;  
     // Set Erase Sectors Opcode 
     if ( false == _i2c_reg_write_one_byte( ___st45LP , FTP_CTRL_1,
                 __u01) ) EXi( -231 ) ; 
-    __cnt ++ ; // Set Soft Prog Opcode 
+    __cnt ++ ; // Set Soft Prog Opcode  // 9 // 0x97 w:0x05
 
 
     __u01 =FTP_CUST_PWR | FTP_CUST_RST_N | FTP_CUST_REQ ;  
     // Load Erase Sectors Opcode 
     if ( false == _i2c_reg_write_one_byte( ___st45LP , FTP_CTRL_0,
                 __u01) ) EXi( -232 ) ; 
-    __cnt ++ ; // Set Soft Prog Opcode 
+    __cnt ++ ; // Set Soft Prog Opcode  // 10 // 0x96 w:0xD0
 
 
     do 
     {
-        // Wait for execution 3
+        // Wait for execution 3 // // read from 0x96
         if ( NULL == _i2c_reg_read_one_byte( ___st45LP , FTP_CTRL_0 ) ) EXi( -234 );
     }
-    while(___st45LP->rBuf[0]  & FTP_CUST_REQ);	
+    while(___st45LP->rBuf[0]  & FTP_CUST_REQ);	// read 0xD0,0xD0,0xD0, failed ; 0xC0, ok-exit loop
 
+    if(1) printf( "now __cnt is %d \n" , __cnt ) ;
     return __cnt;
 
 } // _st45_enter_nvm_write
